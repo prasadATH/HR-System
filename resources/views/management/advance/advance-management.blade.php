f40e752e3f1c4fabfeed5294a6b5a1b9cf09a5fa
Thilina Prasad Athukorala <thilinaprasad603@gmail.com>
2025-01-01 15:32:58 +0530

    commit

--- resources/views/management/advance/advance-management.blade.php
+++ resources/views/management/advance/advance-management.blade.php
@@ -97,7 +97,12 @@
     <p class="md:text-6xl text-4xl font-bold text-black nunito-">Advance</p>
     </div>
     <div class="flex items-center space-x-4">
-
+    <!-- Filter Button -->
+    <button class="flex items-center justify-between px-4 md:py-2 py-1 text-[#00000066] md:text-2xl text-xl bg-[#D9D9D980] border-2 border-[#D9D9D980] rounded-md hover:bg-gray-200">
+        <p class="text-3xl"><i class="ri-filter-2-line"></i></p>
+        <span>Filter</span>
+        <p class="text-3xl text-[#00000066]"><i class="ri-arrow-down-s-line"></i></p>
+    </button>
 
     <!-- Add Employee Button -->   
     <button class="flex items-center justify-center space-x-2 px-10 py-2 text-white md:text-2xl text-xl bg-gradient-to-r from-[#184E77] to-[#52B69A] rounded-xl shadow-sm hover:from-[#1B5A8A] hover:to-[#60C3A8]" onclick="openAddModal()">
